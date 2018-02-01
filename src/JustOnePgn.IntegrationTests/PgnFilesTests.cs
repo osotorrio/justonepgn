ee50746aeb00e64e70cc0c6947b15b6ff4867c70
osotorrio <osotorrio@hotmail.com>
2018-02-01 21:24:13 +0000

    Set up integration test project

---
+++ src/JustOnePgn.IntegrationTests/PgnFilesTests.cs
@@ -0,0 +1,17 @@
+﻿using Xbehave;
+
+namespace JustOnePgn.IntegrationTests
+{
+    public class PgnFilesTests
+    {
+        [Scenario]
+        public void OneFileOneGameTest()
+        {
+            "GIVEN a folder with one PGN file".x(() => { });
+            "AND the file contains one single game".x(() => { });
+            "WHEN the file is processed".x(() => { });
+            "THEN one single PGN file is created".x(() => { });
+            "AND the file contains one game".x(() => { });
+        }
+    }
+}
