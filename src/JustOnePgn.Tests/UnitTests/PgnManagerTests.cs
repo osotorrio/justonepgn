5eed2f8e45bc5045471f47259f227592e8872a30
osotorrio <osotorrio@hotmail.com>
2018-03-04 17:59:55 +0000

    Unit Test. PgnManager tests related to pgn reader and writer

---
+++ src/JustOnePgn.Tests/UnitTests/PgnManagerTests.cs
@@ -0,0 +1,45 @@
+﻿using JustOnePgn.Core.Contracts;
+using JustOnePgn.Core.Domain;
+using JustOnePgn.Core.Services;
+using NSubstitute;
+using System;
+using Xunit;
+
+namespace JustOnePgn.Tests.UnitTests
+{
+    public class PgnManagerTests
+    {
+        [Fact]
+        public void Execute_should_call_ReadGame()
+        {
+            // Arrange
+            var readerMock = Substitute.For<IReadPgnFiles>();
+            var writerStub = Substitute.For<IWritePgnFiles>();
+            var manager = new PgnManager(readerMock, writerStub);
+
+            // Act
+            manager.Execute();
+
+            // Assert
+            readerMock.Received().ReadGame(Arg.Any<Action<Game>>());
+        }
+
+        [Fact]
+        public void Exectute_should_call_WriteGame_with_a_Game()
+        {
+            // Arrange
+            var expectedGame = new Game();
+            var writerMock = Substitute.For<IWritePgnFiles>();
+            var readerStub = Substitute.For<IReadPgnFiles>();
+            readerStub.ReadGame(Arg.Invoke(expectedGame));
+
+            var manager = new PgnManager(readerStub, writerMock);
+
+            // Act
+            manager.Execute();
+
+            // Assert
+            writerMock.Received().WriteGame(expectedGame);
+        }
+    }
+}
