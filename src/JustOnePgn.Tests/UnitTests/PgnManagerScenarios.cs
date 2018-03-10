97b6833b8d8d3c54a2c1d11ea4b801fa2d7c96ad
osotorrio <osotorrio@hotmail.com>
2018-03-10 18:56:55 +0000

    Moved to use scenarios for PgnManager

---
+++ src/JustOnePgn.Tests/UnitTests/PgnManagerScenarios.cs
@@ -0,0 +1,93 @@
+﻿using JustOnePgn.Core.Contracts;
+using JustOnePgn.Core.Domain;
+using JustOnePgn.Core.Services;
+using NSubstitute;
+using Xbehave;
+
+namespace JustOnePgn.Tests.UnitTests
+{
+    public class PgnManagerScenarios
+    {
+        [Scenario]
+        public void AlwaysWriteGameTest(TestFixture fixture, Game game, IPgnManager manager)
+        {
+            "GIVEN a game".x(() => 
+            {
+                fixture = new TestFixture();
+                game = new Game();
+            });
+
+            "WHEN the game gets read".x(() => 
+            {
+                fixture.FakeReader.ReadGame(Arg.Invoke(game));
+            });
+
+            "AND the game is processed".x(() => 
+            {
+                manager = new PgnManager(fixture.FakeReader, fixture.FakeWriter, fixture.FakeRepo);
+                manager.Execute();
+            });
+
+            "THEN the same game is written".x(() => 
+            {
+                fixture.FakeWriter.Received().WriteGame(Arg.Is(game));
+            });
+        }
+
+        [Scenario]
+        public void NotDuplicatedGameTest(TestFixture fixture, Game game, IPgnManager manager)
+        {
+            "GIVEN a game".x(() => 
+            {
+                fixture = new TestFixture();
+
+                game = new Game();
+                fixture.FakeReader.ReadGame(Arg.Invoke(game));
+            });
+
+            "AND the game is not duplicated".x(() =>
+            {
+                fixture.FakeRepo.IsDuplicatedGame(game).Returns(false);
+            });
+
+            "WHEN the game is processed".x(() =>
+            {
+                manager = new PgnManager(fixture.FakeReader, fixture.FakeWriter, fixture.FakeRepo);
+                manager.Execute();
+            });
+
+            "THEN the game is stored in the database".x(() =>
+            {
+                fixture.FakeRepo.Received().SaveGame(Arg.Is(game));
+            });
+        }
+
+        [Scenario]
+        public void DuplicatedGameTest(TestFixture fixture, Game game, IPgnManager manager)
+        {
+            "GIVEN a game".x(() =>
+            {
+                fixture = new TestFixture();
+
+                game = new Game();
+                fixture.FakeReader.ReadGame(Arg.Invoke(game));
+            });
+
+            "AND the game is duplicated".x(() =>
+            {
+                fixture.FakeRepo.IsDuplicatedGame(game).Returns(true);
+            });
+
+            "WHEN the game is processed".x(() =>
+            {
+                manager = new PgnManager(fixture.FakeReader, fixture.FakeWriter, fixture.FakeRepo);
+                manager.Execute();
+            });
+
+            "THEN the game is not stored in the database".x(() =>
+            {
+                fixture.FakeRepo.DidNotReceive().SaveGame(Arg.Is(game));
+            });
+        }
+    }
+}
---
+++ src/JustOnePgn.Tests/UnitTests/TestFixture.cs
@@ -0,0 +1,26 @@
+﻿using JustOnePgn.Core.Contracts;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustOnePgn.Tests.UnitTests
+{
+    public class TestFixture
+    {
+        public IReadPgnFiles FakeReader { get; private set; }
+
+        public IWritePgnFiles FakeWriter { get; private set; }
+
+        public IGameRepository FakeRepo { get; private set; }
+
+        public TestFixture()
+        {
+            FakeReader = Substitute.For<IReadPgnFiles>();
+            FakeWriter = Substitute.For<IWritePgnFiles>();
+            FakeRepo = Substitute.For<IGameRepository>();
+        }
+    }
+}
