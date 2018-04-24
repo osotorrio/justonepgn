78605b2868ae8c70d4c2fd10c7266001dc03003c
osotorrio <osotorrio@hotmail.com>
2018-04-24 04:29:24 +0100

    Modified file names

--- src/JustOnePgn.Client/Program.cs
+++ src/JustOnePgn.Client/Program.cs
@@ -14,9 +14,9 @@ namespace JustOnePgn.Client
             _start = DateTime.UtcNow;
 
             var reader = new PgnReader(@"C:\Chess\PNGSources");
-            var writer = new PgnWriter(@"C:\Chess\Databases\db2018_v2.pgn");
+            var writer = new PgnWriter(@"C:\Chess\Databases\db2018.pgn");
             var repo = new GameRepository(@"Data Source=LENOVO-PC;Initial Catalog=PlayGrandmasters;Integrated Security=True");
-            var logger = new Logger(@"C:\Chess\Databases\logs_v2.txt");
+            var logger = new Logger(@"C:\Chess\Databases\logs.txt");
 
             var manager = new PgnManager(reader, writer, repo, logger);
 
